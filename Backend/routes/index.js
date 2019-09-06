2bcf8b9b58eebc0b52254901e198929ca34df3c3
chaharshivam <chahar.shivam@yahoo.com>
2019-09-06 20:06:50 +0530

    Update tags route

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Tag = require('../models/tags');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET popular tags */
+router.get('/tags', function(req, res, next) {
+  Tag.find({}, (err, tags) => {
+    if (err) return next(err);
+
+    tags = tags.sort((a, b) => {
+      return a.articles.length - b.articles.length;
+    }).slice(0, 10);
+
+    return res.status(200).json({ tags });
+  });
 });
 
 module.exports = router;
