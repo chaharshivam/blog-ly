0f2f03ab97f950b4cbfbb100417e99234de47b78
chaharshivam <chahar.shivam@yahoo.com>
2019-09-26 21:45:32 +0530

    Add signup/login dialog component

---
+++ Frontend/src/components/signIn-signup/Dialog.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Login from './Login';
+import Signup from './Signup';
+
+class Dialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dialog: this.props.dialog
+        }
+    }
+
+    handleClick = dialog => {
+        this.setState({dialog});
+    }
+
+    render() {
+        return (
+            <div className="dialog flex-center wrapper">
+                <div className="dialog-wrapper">
+                    <header className="dialog-header flex-center">
+                        <button 
+                            className={`btn-dialog ${this.state.dialog === 'login' ? '' : 'inactive-dialog'}`} 
+                            onClick={() => this.handleClick('login')}
+                        >
+                            Login
+                        </button>
+                        <button 
+                            className={`btn-dialog ${this.state.dialog === 'signup' ? '' : 'inactive-dialog'}`}
+                            onClick={() => this.handleClick('signup')}
+                        >
+                            Signup
+                        </button>
+                    </header>
+                    <main className="flex-center">
+                        {
+                            this.state.dialog === 'login' ? <Login /> : <Signup />
+                        }
+                    </main>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dialog;
