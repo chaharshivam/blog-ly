6c967626be3ee8c34e2176dd8a450d358ff46ae9
chaharshivam <chahar.shivam@yahoo.com>
2019-09-27 16:17:51 +0530

    Add tags component

--- Frontend/src/components/Feed/Feed.js
+++ Frontend/src/components/Feed/Feed.js
@@ -1,49 +1,54 @@
 import React from 'react';
 import ArticleDialog from '../Article/ArticleDialog';
+import Tags from './Tags';
 
-function Feed(props) {
-    return (
-        <main className="feed-container">
-            <div className="wrapper flex-between">
-                <aside className="feed">
-                    <header className="feed-header">
-                        <a className="feed-btn">Global Feed</a>
-                    </header>
-                    <main>
-                        <ArticleDialog />
-                        <ArticleDialog />
-                        <ArticleDialog />
-                        <ArticleDialog />
-                        <ArticleDialog />
-                    </main>
-                    <footer>
-                        <div className="paginate flex-start">
-                            <a href="#" className="page current-page">
-                                <button>1</button>
-                            </a>
-                            <a href="#" className="page">
-                                <button>2</button>
-                            </a>
-                            <a href="#" className="page">
-                                <button>3</button>
-                            </a>
-                        </div>
-                    </footer>
-                </aside>
-                <aside className="popular-tags-container">
-                    <h5>Popular tags</h5>
-                    <div className="popular-tags flex-start">
-                        <span className="popular-tag">nodejs</span>
-                        <span className="popular-tag">react</span>
-                        <span className="popular-tag">javascript</span>
-                        <span className="popular-tag">c++</span>
-                        <span className="popular-tag">java</span>
-                        <span className="popular-tag">api</span>
-                    </div>
-                </aside>
-            </div>
-        </main>
-    );
+class Feed extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/tags')
+        .then(res => res.json())
+        .then(tags => this.setState({ tags: tags.tags }));
+    }
+
+    render() {
+        return (
+            <main className="feed-container">
+                <div className="wrapper flex-between">
+                    <aside className="feed">
+                        <header className="feed-header">
+                            <a className="feed-btn">Global Feed</a>
+                        </header>
+                        <main>
+                            <ArticleDialog />
+                        </main>
+                        <footer>
+                            <div className="paginate flex-start">
+                                <a href="#" className="page current-page">
+                                    <button>1</button>
+                                </a>
+                                <a href="#" className="page">
+                                    <button>2</button>
+                                </a>
+                                <a href="#" className="page">
+                                    <button>3</button>
+                                </a>
+                            </div>
+                        </footer>
+                    </aside>
+                    <aside className="popular-tags-container">
+                        <h5>Popular tags</h5>
+                        <Tags />
+                    </aside>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default Feed;
---
+++ Frontend/src/components/Feed/Tags.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Tags extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/tags')
+        .then(res => res.json())
+        .then(tags => this.setState({ tags: tags.tags }));
+    }
+
+    render() {
+        return (
+            <div className="popular-tags flex-start">
+                {
+                    this.state.tags && this.state.tags.map(tag => <span className="popular-tag">{tag.name}</span>)
+                }
+            </div>
+        );
+    }
+
+}
+
+export default Tags;
