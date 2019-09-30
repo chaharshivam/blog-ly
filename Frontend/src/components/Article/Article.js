4438815dfc7e971293d55a77c75c186f570bc2d8
chaharshivam <chahar.shivam@yahoo.com>
2019-09-30 22:50:02 +0530

    Add Article component

---
+++ Frontend/src/components/Article/Article.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+class Article extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            article: null
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:3001/api/${this.props.match.url}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `${ localStorage.authToken }`
+            }
+        })
+        .then(res => res.json())
+        .then( ({ article }) => this.setState({ article }))
+        .catch(err => console.log(err) )
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+               {
+                   this.state.article &&
+
+                   (
+                   <React.Fragment>
+                    <section className="article">
+                            <div className="wrapper">
+                                <header className="article-header">
+                                    <h2>{this.state.article.title}</h2>
+                                    <div className="flex-start article-card">
+                                        <div className="author flex-start">
+                                            <img className="author-avatar" src="https://static.productionready.io/images/smiley-cyrus.jpg"></img>
+                                            <div className="article-details flex-start">
+                                                <a>
+                                                    <span className="author-username">{this.state.article.author.username}</span>
+                                                </a>
+                                                <span className="article-created-on">{new Date(this.state.article.createdAt).toDateString()}</span>
+                                            </div>
+                                        </div>
+                                        <button className="follow-btn">
+                                            <span>+</span>
+                                            <span> Follow {this.state.article.author.username}</span>
+                                        </button>
+                                        <button className="like-btn">
+                                            <span className="heart">♥</span>
+                                            <span className="like-count">{this.state.article.likes}</span>
+                                        </button>
+                                    </div>
+                                </header>
+                            </div>
+                        </section>
+                        <section className="article-description">
+                            <div 
+                                className="wrapper"
+                                dangerouslySetInnerHTML={{ __html: this.state.article.description }}
+                            >
+
+                            </div>
+                        </section>
+                   </React.Fragment>
+                )
+               }
+            </React.Fragment>
+        );
+    }
+}
+
+export default Article;
