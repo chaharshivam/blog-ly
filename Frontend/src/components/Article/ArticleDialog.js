ad1830cb7278f77db8914619820c1cb4b20b25cf
chaharshivam <chahar.shivam@yahoo.com>
2019-10-02 23:22:34 +0530

    Update Article Feeds

--- Frontend/src/components/Article/ArticleDialog.js
+++ Frontend/src/components/Article/ArticleDialog.js
@@ -4,28 +4,14 @@ import ArticleCard from './ArticleCard';
 class ArticleDialog extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            articles: null
-        }
-    }
-
-    componentDidMount() {
-        fetch('http://localhost:3001/api/articles')
-        .then(res => res.json())
-        .then(data => this.setState({ articles: data.articles }));
     }
 
     render() {
         return (  
-            this.state.articles && this.state.articles.map(article => {
+            this.props.articles && this.props.articles.map(article => {
                 return (<ArticleCard
                     isUser={this.props.isUser}
-                    author={article.author.username}
-                    createdAt={new Date(article.createdAt).toDateString()}
-                    likes={article.likes}
-                    title={article.title}
-                    description={article.description}
-                    slug={article.slug}
+                    article={article}
                 />)
             })      
         );
